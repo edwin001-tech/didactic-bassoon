00917dfb4b2ab5489b661917bc5eca3237ca5ed0
edwin001-tech <edwinshitakwa8@gmail.com>
2020-08-10 15:14:40 +0300

    create user and authenticated user

--- app/src/main/java/com/example/self/CreateAccountActivity.java
+++ app/src/main/java/com/example/self/CreateAccountActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -18,6 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -35,6 +37,9 @@ public class CreateAccountActivity extends AppCompatActivity {
 
     //Firestore connection
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private CollectionReference collectionReference =  db.collection("Users");
+
 
     private EditText emailEditText;
     private EditText passwordEditText;
@@ -78,7 +83,23 @@ public class CreateAccountActivity extends AppCompatActivity {
         createAcctButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createUserEmailAccount(email, password, username);
+                if (!TextUtils.isEmpty(emailEditText.getText().toString())
+                        && !TextUtils.isEmpty(passwordEditText.getText().toString())
+                        && !TextUtils.isEmpty(userNameEditText.getText().toString())) {
+
+                    String email = emailEditText.getText().toString().trim();
+                    String password = passwordEditText.getText().toString().trim();
+                    String username = userNameEditText.getText().toString().trim();
+
+                    createUserEmailAccount(email, password, username);
+
+                }else {
+                    Toast.makeText(CreateAccountActivity.this,
+                            "Empty Fields Not Allowed",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+
             }
         });
     }
@@ -106,46 +127,46 @@ public class CreateAccountActivity extends AppCompatActivity {
                                 userObj.put("username", username);
 
                                 //save to our firestore database
-//                                collectionReference.add(userObj)
-//                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                                            @Override
-//                                            public void onSuccess(DocumentReference documentReference) {
-//                                                documentReference.get()
-//                                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//                                                            @Override
-//                                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                                                                if (Objects.requireNonNull(task.getResult()).exists()) {
-//                                                                    progressBar.setVisibility(View.INVISIBLE);
-//                                                                    String name = task.getResult()
-//                                                                            .getString("username");
+                                collectionReference.add(userObj)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                documentReference.get()
+                                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                                if (Objects.requireNonNull(task.getResult()).exists()) {
+                                                                    progressBar.setVisibility(View.INVISIBLE);
+                                                                    String name = task.getResult()
+                                                                            .getString("username");
 //
 //                                                                    JournalApi journalApi = JournalApi.getInstance(); //Global API
 //                                                                    journalApi.setUserId(currentUserId);
 //                                                                    journalApi.setUsername(name);
-//
-//                                                                    Intent intent = new Intent(CreateAccountActivity.this,
-//                                                                            PostJournalActivity.class);
-//                                                                    intent.putExtra("username", name);
-//                                                                    intent.putExtra("userId", currentUserId);
-//                                                                    startActivity(intent);
-//
-//
-//                                                                }else {
-//
-//                                                                    progressBar.setVisibility(View.INVISIBLE);
-//                                                                }
-//
-//                                                            }
-//                                                        });
-//
-//                                            }
-//                                        })
-//                                        .addOnFailureListener(new OnFailureListener() {
-//                                            @Override
-//                                            public void onFailure(@NonNull Exception e) {
-//
-//                                            }
-//                                        });
+
+                                                                    Intent intent = new Intent(CreateAccountActivity.this,
+                                                                            PostJournalActivity.class);
+                                                                    intent.putExtra("username", name);
+                                                                    intent.putExtra("userId", currentUserId);
+                                                                    startActivity(intent);
+
+
+                                                                }else {
+
+                                                                    progressBar.setVisibility(View.INVISIBLE);
+                                                                }
+
+                                                            }
+                                                        });
+
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+
+                                            }
+                                        });
 
 
                             }else {
