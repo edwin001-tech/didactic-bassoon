ca189f6ae0716aad0575c481d3691705a55e0a40
edwin001-tech <edwinshitakwa8@gmail.com>
2020-08-10 16:27:33 +0300

    Journal Post Screen

--- app/src/main/java/com/example/self/PostJournalActivity.java
+++ app/src/main/java/com/example/self/PostJournalActivity.java
@@ -1,14 +1,54 @@
 package com.example.self;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 public class PostJournalActivity extends AppCompatActivity {
+    private Button saveButton;
+    private ProgressBar progressBar;
+    private ImageView addPhotoButton;
+    private EditText titleEditText;
+    private EditText thoughtsEditText;
+    private TextView currentUserTextView;
+    private ImageView imageView;
+
+    private String currentUserId;
+    private String currentUserName;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
+    //Connection to Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
+
+    private CollectionReference collectionReference = db.collection("Journal");
+    private Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_journal);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.post_progressBar);
+        titleEditText = findViewById(R.id.post_title_et);
+        thoughtsEditText = findViewById(R.id.post_description_et);
+        currentUserTextView = findViewById(R.id.post_username_textview);
     }
 }
