ed23c6fc668bf2b8843dbc2f5079322032d33bf1
edwin001-tech <edwinshitakwa8@gmail.com>
2020-08-10 11:34:18 +0300

    Create Account Activity

---
+++ app/src/main/java/com/example/self/CreateAccountActivity.java
@@ -0,0 +1,19 @@
+package com.example.self;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+
+    }
+}
--- app/src/main/java/com/example/self/LoginActivity.java
+++ app/src/main/java/com/example/self/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.self;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button createAcctButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_sign_in_button);
+        createAcctButton = findViewById(R.id.create_acct_button_login);
+
+        createAcctButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,
+                        CreateAccountActivity.class));
+            }
+        });
     }
 }
