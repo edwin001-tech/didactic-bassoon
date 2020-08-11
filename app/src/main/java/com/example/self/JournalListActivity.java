448e7fab164a35f41b4bd8ac40a14a11100c7b51
edwin001-tech <edwinshitakwa8@gmail.com>
2020-08-11 16:08:51 +0300

    added menu icons to list activity

--- app/src/main/java/com/example/self/JournalListActivity.java
+++ app/src/main/java/com/example/self/JournalListActivity.java
@@ -1,7 +1,10 @@
 package com.example.self;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JournalListActivity extends AppCompatActivity {
@@ -10,5 +13,28 @@ public class JournalListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_add:
+                //take users to add journal
+                break;
+            case R.id.action_signout:
+                //sign user out
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
