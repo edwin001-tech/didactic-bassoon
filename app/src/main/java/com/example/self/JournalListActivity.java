9e50ab54b6a5c70040a78bc8ccf7b2163f2a5e94
edwin001-tech <edwinshitakwa8@gmail.com>
2020-08-11 16:29:05 +0300

    Logout function

--- app/src/main/java/com/example/self/JournalListActivity.java
+++ app/src/main/java/com/example/self/JournalListActivity.java
@@ -1,5 +1,6 @@
 package com.example.self;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -7,12 +8,25 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+
 public class JournalListActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
     }
 
     @Override
@@ -29,12 +43,29 @@ public class JournalListActivity extends AppCompatActivity {
         switch(item.getItemId()){
             case R.id.action_add:
                 //take users to add journal
+                if (user != null && firebaseAuth != null) {
+                    startActivity(new Intent(JournalListActivity.this,
+                            PostJournalActivity.class));
+                    //finish();
+                }
                 break;
             case R.id.action_signout:
                 //sign user out
+                if (user != null && firebaseAuth != null) {
+                    firebaseAuth.signOut();
+
+                    startActivity(new Intent(JournalListActivity.this,
+                            MainActivity.class));
+                    //finish();
+                }
                 break;
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
