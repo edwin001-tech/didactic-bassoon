080b7d774cfeae2fac855a317fa74074d4456fbc
edwin001-tech <edwinshitakwa8@gmail.com>
2020-08-12 17:11:42 +0300

    final commit

--- app/src/main/java/com/example/self/MainActivity.java
+++ app/src/main/java/com/example/self/MainActivity.java
@@ -5,12 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.self.util.JournalApi;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Objects;
 
+import javax.annotation.Nullable;
+
 public class MainActivity extends AppCompatActivity {
     private Button getStartedButton;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db =  FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +40,74 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setElevation(0);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null) {
+                    currentUser = firebaseAuth.getCurrentUser();
+                    final String currentUserId = currentUser.getUid();
+
+                    collectionReference
+                            .whereEqualTo("userId", currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                                    @Nullable FirebaseFirestoreException e) {
+                                    if (e != null) {
+                                        return;
+                                    }
+
+                                    String name;
+
+                                    if (!queryDocumentSnapshots.isEmpty()) {
+                                        for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                            JournalApi journalApi = JournalApi.getInstance();
+                                            journalApi.setUserId(snapshot.getString("userId"));
+                                            journalApi.setUsername(snapshot.getString("username"));
+
+                                            startActivity(new Intent(MainActivity.this,
+                                                    JournalListActivity.class));
+                                            finish();
+
+
+                                        }
+                                    }
+
+                                }
+                            });
+
+                }else {
+
+                }
+            }
+        };
+
+
         getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //go to Login Screen
                 startActivity(new Intent(MainActivity.this,
                 LoginActivity.class));
+                finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
